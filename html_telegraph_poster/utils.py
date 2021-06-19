00f635d48fff1633da12384aad23f5d3c0e74ee2
mercuree <mercuree.lab@gmail.com>
2021-06-19 17:47:15 +0300

    fixed and optimized url conversion

--- html_telegraph_poster/utils.py
+++ html_telegraph_poster/utils.py
@@ -1,9 +1,12 @@
 # coding=utf8
 
-from urllib.parse import urlparse
+from urllib.parse import urlparse, urljoin
+import logging
 from .upload_images import upload_image
 from .converter import _fragments_from_string
 import lxml.html
+
+LOG = logging.getLogger(__name__)
 
 
 class DocumentPreprocessor:
@@ -11,23 +14,23 @@ class DocumentPreprocessor:
         self.input_html = input_html
         self.parsed_document = self._parse_document()
 
-    def _parse_document(self):
-        fragments = _fragments_from_string(self.input_html)
-        document = fragments[0].xpath('/*')[0] if len(fragments) else None
-        return document
-
     def get_processed_html(self):
         return lxml.html.tostring(self.parsed_document, encoding='unicode')
 
     def upload_all_images(self, base_url=None):
-        self._make_links_absoulte(base_url)
+        self._make_links_absolute(base_url)
         images = self.parsed_document.xpath('.//img[@src]')
         for image in images:
             old_image_url = image.attrib.get('src')
             new_image_url = upload_image(old_image_url)
             image.attrib.update({'src': new_image_url})
 
-    def _make_links_absoulte(self, base_url=None):
+    def _parse_document(self):
+        fragments = _fragments_from_string(self.input_html)
+        document = fragments[0].xpath('/*')[0] if len(fragments) else None
+        return document
+
+    def _make_links_absolute(self, base_url=None):
 
         body = self.parsed_document.body
         output_base = None
@@ -40,14 +43,19 @@ class DocumentPreprocessor:
         elif document_base_url:
             if urlparse(document_base_url).netloc:
                 output_base = document_base_url
-            else:
-                # base url contains relative path only
-                # we should print the warning
-                pass
-        else:
-            # no base_url was passed, document_base_url is missing
-            # put another warning?
-            # setting invalid base_url will force to discard url, empty image then will be remove by the cleaner
-            output_base = '//['
 
-        body.make_links_absolute(output_base, handle_failures='discard')
+        if output_base is None:
+            # no base_url was passed, document_base_url is missing
+            LOG.warning('Relative image/link urls were removed from the document')
+
+        def link_replace(href):
+            try:
+                if output_base is None and not urlparse(href).netloc:
+                    url = None
+                else:
+                    url = urljoin(output_base, href)
+                return url
+            except ValueError:
+                return None
+
+        body.rewrite_links(link_repl_func=link_replace, base_href=output_base)
