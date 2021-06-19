30cad0739b2cb03132f9882ad84d77fb5d025035
mercuree <mercuree.lab@gmail.com>
2021-06-19 17:47:15 +0300

    fixed and optimized url conversion

--- html_telegraph_poster/__init__.py
+++ html_telegraph_poster/__init__.py
@@ -1,2 +1,5 @@
 from .html_to_telegraph import upload_to_telegraph, TelegraphPoster
 from .upload_images import upload_image
+import logging
+
+logging.getLogger(__name__).addHandler(logging.NullHandler())
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -19,18 +19,22 @@ class DocumentPreprocessorImageUploadTest(unittest.TestCase):
                 {bodycontent} 
             </body>
         </html>'''
-
-    def test_upload_all_images(self):
-        html_images = self.document.format(headcontent='', bodycontent='''
+        self.html_figures = '''
             <figure>
-                <img src="http://httpbin.org/image/jpeg" />
+                <img src="{0}" />
                 <figcaption> image caption</figcaption>
             </figure>
             <figure>
-                <img src="http://httpbin.org/image/png" />
+                <img src="{1}" />
                 <figcaption>image caption</figcaption>
             </figure>
-        ''')
+        '''
+
+    def test_upload_all_images(self):
+        html_images = self.document.format(
+            headcontent='',
+            bodycontent=self.html_figures.format('http://httpbin.org/image/jpeg', 'http://httpbin.org/image/png')
+        )
 
         dp = DocumentPreprocessor(html_images)
         dp.upload_all_images()
@@ -45,46 +49,48 @@ class DocumentPreprocessorImageUploadTest(unittest.TestCase):
     def test_make_links_absolute_document_base_url(self):
         html_images = self.document.format(
             headcontent='<base href="http://httpbin.org/">',
-            bodycontent='''
-            <figure>
-                <img src="image/jpeg" />
-                <figcaption> image caption</figcaption>
-            </figure>
-            <figure>
-                <img src="image/png" />
-                <figcaption>image caption</figcaption>
-            </figure>
-        ''')
+            bodycontent=self.html_figures.format('image/jpeg', 'image/png')
+        )
 
         dp = DocumentPreprocessor(html_images)
-        dp._make_links_absoulte()
+        dp._make_links_absolute()
+        processed_html = dp.get_processed_html()
         self.assertTrue(
-            '<img src="http://httpbin.org/image/png">' in dp.get_processed_html()
+            '<img src="http://httpbin.org/image/png">' in processed_html
         )
         self.assertTrue(
-            '<img src="http://httpbin.org/image/jpeg">' in dp.get_processed_html()
+            '<img src="http://httpbin.org/image/jpeg">' in processed_html
         )
 
     def test_make_links_absolute_document_without_base_url(self):
         html_images = self.document.format(
             headcontent='',
-            bodycontent='''
-            <figure>
-                <img src="image/jpeg" />
-                <figcaption> image caption</figcaption>
-            </figure>
-            <figure>
-                <img src="image/png" />
-                <figcaption>image caption</figcaption>
-            </figure>
-        ''')
-
+            bodycontent=self.html_figures.format('image/jpeg', 'image/png')
+        )
         dp = DocumentPreprocessor(html_images)
-        dp._make_links_absoulte()
-        print(dp.get_processed_html())
+        dp._make_links_absolute()
+        processed_html = dp.get_processed_html()
         self.assertFalse(
-            '<img src' in dp.get_processed_html()
+            '<img src' in processed_html
         )
         self.assertFalse(
-            '<img src' in dp.get_processed_html()
+            '<img src' in processed_html
+        )
+
+    def test_make_links_absolute_pass_base_url(self):
+        html_images = self.document.format(
+            headcontent='',
+            bodycontent='<a href="test_link.html">test Link</a>' + self.html_figures.format('image/jpeg', 'image/png')
+        )
+        dp = DocumentPreprocessor(html_images)
+        dp._make_links_absolute(base_url='http://httpbin.org')
+        processed_html = dp.get_processed_html()
+        self.assertTrue(
+            '<img src="http://httpbin.org/image/png">' in processed_html
+        )
+        self.assertTrue(
+            '<img src="http://httpbin.org/image/jpeg">' in processed_html
+        )
+        self.assertTrue(
+            '<a href="http://httpbin.org/test_link.html">' in processed_html
         )
